c3aff904ff974911aa0647d89d569b2b025a8c80
Diego Becerra <becerra.webd@gmail.com>
2020-06-02 19:04:38 -0300

    Updates

---
+++ precache-manifest.f83272380e93dd7f0d43d8f9b28aa122.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e883c4ea0ca79729f9682ad4f8f318",
+    "url": "/social-media-dashboard/index.html"
+  },
+  {
+    "revision": "17f0c1a97aacc8956258",
+    "url": "/social-media-dashboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "89f5f056c47589e76dfb",
+    "url": "/social-media-dashboard/static/js/2.81da7c6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-media-dashboard/static/js/2.81da7c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f0c1a97aacc8956258",
+    "url": "/social-media-dashboard/static/js/main.98abdc11.chunk.js"
+  },
+  {
+    "revision": "4138ac29099c3aab0dfd",
+    "url": "/social-media-dashboard/static/js/runtime-main.e545d757.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/social-media-dashboard/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/social-media-dashboard/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/social-media-dashboard/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/social-media-dashboard/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/social-media-dashboard/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/social-media-dashboard/static/media/icon-youtube.8499da12.svg"
+  }
+]);
